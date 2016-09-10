978e2a9b3180bb23f813e33d3f6e59b1d817c66e
Ori Riner <ori.riner@autodesk.com>
2016-09-10 12:15:55 +0300

    Add player and ball

--- game.js
+++ game.js
@@ -6,6 +6,8 @@ window.onload = function(){
     var brickMargin = 10;
     var playerBaseLine = 50;
     var ballRadius = 10;
+    var maxNumberOfLives = 3;
+    var duration = 5;
     var lives = [];
     var bricks = [];
     var boardWidth;
@@ -26,6 +28,14 @@ window.onload = function(){
      * Resets the style of the canvas context for drawing
      */
     function resetContextStyle(){
+      context.fillStyle = 'black';
+      context.strokeStyle = 'black';
+    }
+
+    /**
+     * Puts a given value at the end of the given array and also in the objects array.
+     */
+    function pushTo(array, value){
 
     }
 
@@ -41,14 +51,80 @@ window.onload = function(){
      * Creates a new ball object with initial values
      */
     function createBall(){
+      return {
+        x: boardWidth / 2,
+        y: boardHeight - playerBaseLine,
+        radius: ballRadius,
+        speedx: 0,
+        speedy: 0,
+        render: function(){
+          context.beginPath();
+          context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+          context.fill();
+        },
+        update: function(){
+          if(!hasStarted)
+            return;
 
+          this.x += this.speedx;
+          this.y += this.speedy;
+        },
+        setSpeed: function(x, y){
+          this.speedx = x;
+          this.speedy = y;
+        }
+      };
     }
 
     /**
      * Creates a new player object (the paddle) with initial values
      */
     function createPlayer() {
+      return {
+        x: boardWidth / 2,
+        y: boardHeight - (playerBaseLine - ballRadius),
+        width: 100,
+        height: 10,
+        speedX: 0,
+        isInBoard: true,
+        render: function(){
+          context.fillRect(this.x - this.width / 2, this.y, this.width, this.height);
+        },
+        bottom: function(){
+          return this.y + this.height;
+        },
+        top: function(){
+          return this.y;
+        },
+        left: function(){
+          return this.x - this.width / 2;
+        },
+        right: function(){
+          return this.x + this.width / 2;
+        },
+        update: function(){
+          this.x += this.speedX;
 
+          if(this.x + this.width / 2 >= boardWidth){
+            this.x = boardWidth - this.width / 2;
+            this.setSpeed(0);
+          }
+
+          if(this.x - this.width / 2 <= 0){
+            this.x = this.width / 2;
+            this.setSpeed(0);
+          }
+        },
+        setSpeed: function(x){
+          if(x !== 0){
+            var direction = Math.abs(x)/x;
+            this.speedX = Math.floor(Math.min(Math.abs(x), 35)) * direction;
+          }
+          else{
+            this.speedX = 0;
+          }
+        }
+      };
     }
 
     /**
@@ -70,14 +146,21 @@ window.onload = function(){
      * Clears the entire canvas for redrawing
      */
     function clearCanvas(){
-
+      context.fillStyle = 'white';
+      context.fillRect(0, 0, canvas.width, canvas.height);
+      resetContextStyle();
+      context.strokeRect(0, 0, boardWidth, boardHeight);
     }
 
     /**
      * Refreshes the game to the new frame on the canvas
      */
     function refresh(){
-
+      clearCanvas();
+      objects.forEach(function(object){
+        object.render();
+        object.update();
+      });
     }
 
     // Describing all the actions that the player can do in the game
@@ -110,7 +193,7 @@ window.onload = function(){
      * Handles a keyup event and what happens each time
      */
     window.addEventListener('keyup', function(event){
-      
+
     });
 
     /**
@@ -121,18 +204,41 @@ window.onload = function(){
     }
 
     /**
+     * Add lives to the canvas (not the board!)
+     */
+    function addLives(){
+
+    }
+
+    /**
      * Resets the player and the ball to their starting positions.
      * Also, when given a number of lives, creates the appropriate amount of hearts for the player.
      */
     function resetPlayerAndBall(numberOfLives){
+      hasStarted = false;
+      ball = createBall();
+      player = createPlayer();
 
+      objects.shift();
+      objects.shift();
+      objects.unshift(ball);
+      objects.unshift(player);
     }
 
     /**
      * Initialize the game loop so that it will draw a new frame and update everything.
      */
     function init(){
+      // Placeholders for the player and ball.
+      objects = [{}, {}];
+      resetPlayerAndBall();
+      addBricks();
 
+      if (gameLoopHandler){
+        clearInterval(gameLoopHandler);
+      }
+
+      gameLoopHandler = setInterval(refresh, 20);
     }
 
     // Start the game!
